d3d91ac14015d2459c434e07b1d957d93636b8cd
Rakshitha Arun Kumar <rakshitha.two3@gmail.com>
2025-10-11 00:30:13 -0700

    Updated all JS files to show output on webpage and improved index.html layout

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,10 +1,11 @@
 (function () {
   const aStr = prompt("Enter the first integer:");
   const bStr = prompt("Enter the second integer:");
-  const a = parseInt(aStr, 10);
-  const b = parseInt(bStr, 10);
 
-  if (Number.isNaN(a) || Number.isNaN(b)) {
+  const a = Number(aStr);
+  const b = Number(bStr);
+
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
     alert("Please enter valid integers.");
     console.log("Invalid input(s). Received:", aStr, bStr);
     return;
@@ -12,12 +13,9 @@
 
   if (a > b) {
     console.log(String(a));
-    console.log(`Larger number is ${a}.`);
   } else if (b > a) {
     console.log(String(b));
-    console.log(`Larger number is ${b}.`);
   } else {
     console.log(`Both are equal: ${a}`);
-    console.log("Numbers are equal.");
   }
 })();
